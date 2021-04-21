250c7fea76e10c47e45333b02c087e0952b58a19
MesQuentin <77100116+MesQuentin@users.noreply.github.com>
2021-04-21 12:25:21 +0100

    Pour une fin de TP6 qui marche

--- TP Rebirth 6 - Quantifier l'amitié/exo1.cpp
+++ TP Rebirth 6 - Quantifier l'amitié/exo1.cpp
@@ -76,27 +76,26 @@ void Graph::wideTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, boo
     }
 }
 
-bool Graph::detectCycle(GraphNode *first, bool visited[])
+bool Graph::detectCycle(GraphNode * first, bool visited[])
 {
-	/**
-	  Detect if there is cycle when starting from first
-	  (the first may not be in the cycle)
-	  Think about what's happen when you get an already visited node
-	**/
+    /**
+      Detect if there is cycle when starting from first
+      (the first may not be in the cycle)
+      Think about what's happen when you get an already visited node
+    **/
 
     visited[first->value]=true;
-    for (Edge* JUSQU_AU_BOUT_DU_MONDE = first->edges; JUSQU_AU_BOUT_DU_MONDE!=NULL; JUSQU_AU_BOUT_DU_MONDE = JUSQU_AU_BOUT_DU_MONDE->next) {
-
-        if (visited[JUSQU_AU_BOUT_DU_MONDE->destination->value]) {
+    for (Edge * e=first->edges ; e!=nullptr ; e=e->next){
+        if (visited[e->destination->value]){
             return true;
         }
 
-        if (not visited[JUSQU_AU_BOUT_DU_MONDE->destination->value]) {
-            detectCycle(JUSQU_AU_BOUT_DU_MONDE->destination, visited);
+        else{
+            if (detectCycle(e->destination, visited)==true){
+                return true;
+            }
         }
     }
-
-    return false;
 }
 
 int main(int argc, char *argv[])
